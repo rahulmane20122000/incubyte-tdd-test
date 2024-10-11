35d902aad7d9c47d57a7bb90642ec84240a248cc
rahul20122000 <rahulmane20122000@gmail.com>
2024-10-11 11:03:03 +0530

    added sum functionality with default and custom delimiters

--- index.ts
+++ index.ts
@@ -3,11 +3,20 @@ const add = (input_string : string)=>{
         if (input_string==="") {
             return 0;
         }
+
+        let delimiter = /,|\n/; // default delimiters
+        if (input_string.startsWith("//")) { // check for custom delimiters
+          const parts = input_string.split("\n"); 
+          delimiter = new RegExp(parts[0][2]);// update delimiter from default to custom 
+          input_string = parts[1]; 
+        }
+      
+        const numArray = input_string.split(delimiter).map(Number); //split using custom delimiter and convert it to number
+        return numArray.reduce((sum, num) => sum + num, 0); // add the numbers and get sum
     } catch (error) {
         throw error;
     }
 }
 
-console.log(add(""))
 
 export default add;
